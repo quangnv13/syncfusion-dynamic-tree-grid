1eb4771ad57fc709edac03f152956f16f9ee3da4
ToanPC <toanpc@ttc-solutions.com.vn>
2022-08-08 09:50:22 +0700

    hot fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { AuthService } from './modules/auth/services/auth.service';
 import { SocketService } from './shared/services/socket.service';
 import { storage } from './shared/utilities/storage';
 
@@ -10,10 +12,24 @@ import { storage } from './shared/utilities/storage';
 })
 export class AppComponent implements OnInit {
   title = 'mgm-poc';
-  constructor(private socketSerice: SocketService) {}
+  constructor(
+    private socketSerice: SocketService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
+    this.authService.currentAccount$.subscribe((token) => {
+      token.length > 0
+        ? this.connectSocketIO(token)
+        : this.router.navigate(['auth/login']);
+    });
+  }
+
+  connectSocketIO(token: string) {
     this.socketSerice.connect(environment.baseSocketIo, {
-      extraHeaders: { Authorization:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InF1YW5nMTMiLCJpYXQiOjE2NTk1OTYzNTh9.hJ62NT3YKEuaCQL-w7ESW_E8oIUs1boCsvK0nbW4ndM'|| storage.get('currentAccount') },
+      extraHeaders: {
+        Authorization: token,
+      },
     });
   }
 }
